09914a72e87e0c2329fcd77bc8a72e0e6143eacf
Joe <Joseph.Williams6501@gmail.com>
2024-09-07 16:11:53 -0500

    Discovered that the Python file being used is different than the one being maintained in Git. Added the correct version in.

--- SageRunner.py
+++ SageRunner.py
@@ -1,127 +1,101 @@
 # This file is meant to be used from within C++ and is thus written
 # to be as easy-to-use as possible from the clunky C++ Python API.
 # As much as possible is hard-coded.
-# import sys
-
-# Because the PyWideStringList_Append function (C++) doesn't actually
-# append, but rather replaces, we need to put the whole path back in place
-# ourselves, here:
-# sys.path = ['', 'C:\\Program Files\\Python310\\python310.zip',
-#          'C:\\Program Files\\Python310\\DLLs',
-#		   'C:\\Program Files\\Python310\\lib',
-#		   'C:\\Program Files\\Python310',
-#		   'C:\\Users\\josep\\AppData\\Roaming\\Python\\Python310\\site-packages',
-#		   'C:\\Program Files\\Python310\\lib\\site-packages',
-#		   'C:\\Users\\josep\\AppData\\Roaming\\Python\\Python310\\Scripts',
-#		   'C:\\Users\\josep\\AppData\\Roaming\\Python\\Python310\\site-packages\\torch\\lib',
-#		   'C:\\Users\\josep\\AppData\\Roaming\\Python\\Library\\bin']
 
 import torch
 import math
 import numpy as np
 import gymnasium as gym
-from pathlib import Path
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env, DummyVecEnv
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 
-# Global variables to make life easy:
-gModel = []
-gEnv = None
-
-# We want a simple 1-environment agent:
-def getEnv(renderMode='None'):
-	return DummyVecEnv([lambda: gym.make("CartPole-v1", render_mode=renderMode)])
-
-# Training is independent and won't be called from C++:
-def train(saveLocation):
-	vec_env = getEnv()
-	eval_env = gym.make("CartPole-v1")
+class SageRunner:
+	def __init__(self):
+		self.useIntAction = True	
 	
-	callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=500, verbose=1)
-	eval_callback = EvalCallback(eval_env, callback_on_new_best=callback_on_best, verbose=1)	
+	# We want a simple 1-environment agent:
+	def getEnv(self, renderMode=None):
+		return DummyVecEnv([lambda: gym.make("CartPole-v1", render_mode=renderMode)])
 
-	model = PPO("MlpPolicy", vec_env, verbose=1)
+	# Training is independent and won't be called from C++:
+	def train(self):
+		vec_env = getEnv()
+		eval_env = gym.make("CartPole-v1")
+	
+		callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=500, verbose=1)
+		eval_callback = EvalCallback(eval_env, callback_on_new_best=callback_on_best, verbose=1)	
 
-	model.learn(total_timesteps=100000, callback=eval_callback)
-	model.save(saveLocation)
+		model = PPO("MlpPolicy", vec_env, verbose=1)
 
-def loadMultiplePPOSages(directory : str):
-	global gModel
-	searchDir = Path(directory)
+		model.learn(total_timesteps=100000, callback=eval_callback)
+		model.save("ppo_sage")
 
-	# Assume every zip file in the folder is a PPO model:
-	for x in searchDir.iterdir():
-	    if str(x).endswith(".zip"):
-		    gModel.append(PPO.load(str(x)[:-4])) # Cut off the ".zip"
+	def load(self):
 
-def load():
-	global gModel
-	global gEnv
+		self.model = PPO.load('ppo_sage')
+		self.env = self.getEnv(renderMode='rgb_array')
 
-	# If there is a folder of sages, load them. If not, load a single sage:
-	if Path("sageCommittee").is_dir():
-		loadMultiplePPOSages("sageCommittee")
-	else:
-		gModel = [PPO.load('ppo_sage')]
+	def render(self):		
+		self.env.render()
 
-	gEnv = getEnv(renderMode='None')
+	def reset(self):
+		# Gather observations and actions:
+		obs = self.env.reset()
+		actions, states = self.model.predict(obs)
+	
+		# We only want the first observation and first action:
+		actions = self.getList(actions[0])
+		obs = self.getList(obs[0])
+		
+		return obs, actions
 
-def reset():
-	global gEnv
-	obs = gEnv.reset()
-	return obs[0]  # Vector environment headaches.
+	# We often need to make sure our return value is a list. Do that here:
+	def getList(self, inputVal):
+		if isinstance(inputVal, np.ndarray):
+			return list(inputVal)
+		else:
+			return list([inputVal])
 
-# Reminder, as a sage it wants to return an observation and what the sage-model
-# would do under those circumstances.
-def step(actualAction: list):
-    global gEnv
-    global gModel
+	# Reminder, as a sage it wants to return an observation and what the sage-model
+	# would do under those circumstances.
+	def step(self, actualAction):
+		if self.useIntAction:
+			actualAction = int(actualAction)
+		
+		inAction = (actualAction, )
+		obs, rewards, dones, info = self.env.step(inAction)
+		actions, states = self.model.predict(obs)
+	
+		obs = self.getList(obs[0])
+		actions = self.getList(actions[0])
+		rewards = rewards[0]
+		dones = dones[0]
+				
+		# Vec-env returns need to be converted to what the singular
+		# agent needs:
+		return (obs, float(rewards), bool(dones), actions)
 
-    inAction = np.array([actualAction])	
-    # inAction = actualAction
-    obs, rewards, dones, info = gEnv.step(inAction)
-    actions = getModelAction(obs)
+	def test(self):
+		vec_env = self.getEnv(renderMode='human')
+		model = PPO.load("ppo_sage")
 
-	# Vec-env returns need to be converted to what the singular
-	# agent needs:
-    return obs[0], rewards[0], dones[0], actions[0]
+		obs = vec_env.reset()
+		maxSteps = 2000
+		# With a simple environment like this, what we want is in subscript
+		# 0 of each of action, rewards, dones, and info.
+		for i in range(maxSteps):
+			action, _states = model.predict(obs)
+			obs, rewards, dones, info = vec_env.step(action)
+			vec_env.render()
+			if dones[0]:
+				print(f"Step {i}: Done. Resetting the environment.")
 
-def getModelAction(obs):
-    global gEnv
-    global gModel
-
-	# Get the first action:
-    action, _ = gModel[0].predict(obs)
-
-	# Add in the rest (possibly none):
-    for i in range(1, len(gModel), 1):
-        tmpAction, _ = gModel[i].predict(obs)
-        action[0] += tmpAction[0]
-
-	# Get the average of the actions as the vote outcome:
-    action[0] = int(round(action[0] / len(gModel), 0))
-
-    return action
-
-def test():
-    vec_env = getEnv(renderMode='human')
-    models = load()
-
-    obs = vec_env.reset()
-    maxSteps = 20001
-    # With a simple environment like this, what we want is in subscript
-    # 0 of each of action, rewards, dones, and info.
-    for i in range(1, maxSteps, 1):
-        action = getModelAction(obs)
-        obs, rewards, dones, info = vec_env.step(action)
-        vec_env.render("human")
-        if dones[0]:
-            print(f"Step {i}: Done. Resetting the environment.")
+def getInstance():
+	return SageRunner()
 
 if __name__ == "__main__":
-    # for i in range(7):
-        # print(f"Training model {i}...")
-        # train(f"sageCommittee\\ppo_sage_{i}")
-    test()
+	# train()
+	# test()
+	print("In Python main.", flush=True)
