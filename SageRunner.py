618030793015783b995afd7df1a3d021bcca871f
Joe <Joseph.Williams6501@gmail.com>
2024-09-07 16:59:14 -0500

    Replaced a single sage with a committee machine of sages, which are much more likely to provide the right answer with any given observation.

--- SageRunner.py
+++ SageRunner.py
@@ -6,6 +6,7 @@ import torch
 import math
 import numpy as np
 import gymnasium as gym
+from pathlib import Path
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env, DummyVecEnv
@@ -13,15 +14,17 @@ from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewar
 
 class SageRunner:
 	def __init__(self):
-		self.useIntAction = True	
+		self.useIntAction = True
+		self.model = []
+		self.env = None
 	
 	# We want a simple 1-environment agent:
 	def getEnv(self, renderMode=None):
 		return DummyVecEnv([lambda: gym.make("CartPole-v1", render_mode=renderMode)])
 
 	# Training is independent and won't be called from C++:
-	def train(self):
-		vec_env = getEnv()
+	def train(self, saveLocation):
+		vec_env = self.getEnv()
 		eval_env = gym.make("CartPole-v1")
 	
 		callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=500, verbose=1)
@@ -30,20 +33,45 @@ class SageRunner:
 		model = PPO("MlpPolicy", vec_env, verbose=1)
 
 		model.learn(total_timesteps=100000, callback=eval_callback)
-		model.save("ppo_sage")
+		model.save(saveLocation)
+
+	def loadMultiplePPOSages(self, directory : str):
+		self.model = []
+		searchDir = Path(directory)
+
+		# Assume every zip file in the folder is a PPO model:
+		for x in searchDir.iterdir():
+			if str(x).endswith(".zip"):
+				self.model.append(PPO.load(str(x)[:-4])) # Cut off the ".zip"
 
 	def load(self):
-
-		self.model = PPO.load('ppo_sage')
+		if Path("sageCommittee").is_dir():
+			self.loadMultiplePPOSages("sageCommittee")
+		else:
+			self.model = [PPO.load('ppo_sage')]
+	
 		self.env = self.getEnv(renderMode='rgb_array')
 
 	def render(self):		
 		self.env.render()
 
+	def getModelAction(self, obs):
+		# Get the first action:
+		action, _ = self.model[0].predict(obs)
+
+		# Add in the rest (possibly no more):
+		for i in range(1, len(self.model), 1):
+			tmpAction, _ = self.model[i].predict(obs)
+			action[0] += tmpAction[0]
+
+		# Get the average as a vote:
+		action[0] = int(round(action[0] / len(self.model), 0))
+		return action
+
 	def reset(self):
 		# Gather observations and actions:
 		obs = self.env.reset()
-		actions, states = self.model.predict(obs)
+		actions = self.getModelAction(obs)
 	
 		# We only want the first observation and first action:
 		actions = self.getList(actions[0])
@@ -66,7 +94,7 @@ class SageRunner:
 		
 		inAction = (actualAction, )
 		obs, rewards, dones, info = self.env.step(inAction)
-		actions, states = self.model.predict(obs)
+		actions = self.getModelAction(obs)
 	
 		obs = self.getList(obs[0])
 		actions = self.getList(actions[0])
@@ -79,14 +107,14 @@ class SageRunner:
 
 	def test(self):
 		vec_env = self.getEnv(renderMode='human')
-		model = PPO.load("ppo_sage")
+		self.load()
 
 		obs = vec_env.reset()
-		maxSteps = 2000
+		maxSteps = 10001
 		# With a simple environment like this, what we want is in subscript
 		# 0 of each of action, rewards, dones, and info.
-		for i in range(maxSteps):
-			action, _states = model.predict(obs)
+		for i in range(1, maxSteps, 1):
+			action = self.getModelAction(obs)
 			obs, rewards, dones, info = vec_env.step(action)
 			vec_env.render()
 			if dones[0]:
@@ -96,6 +124,13 @@ def getInstance():
 	return SageRunner()
 
 if __name__ == "__main__":
-	# train()
-	# test()
+	# runner = getInstance()
+	
+	# Train:
+	# for i in range(7):
+		# train(f"sageCommittee\\ppo_sage_{i}")
+	
+	# Test:
+	# runner.test()
+	
 	print("In Python main.", flush=True)
